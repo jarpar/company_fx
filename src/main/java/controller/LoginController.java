ddfdee21ec53ef452a200c3e460af9b8fc94fd24
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-08-02 09:40:34 +0200

    ref

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -4,17 +4,24 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 public class LoginController {
 
     @FXML
     private TextField tfLogin;
 
+
     @FXML
     private PasswordField pfPassword;
 
     @FXML
     void loginAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void keyLoginAction(KeyEvent event) {
 
     }
 
