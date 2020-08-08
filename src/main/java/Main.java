0b28cc5b1fd6f51abce07823911a7cb015b37c6d
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-08-08 11:08:17 +0200

    ref

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,11 +6,12 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
-    public void init(){
+    public void init() {
         System.out.println("METODA INIT");
     }
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         System.out.println("METODA START");
         // załadowanie widoku z pliku fxml do obiektu parent
         Parent parent = FXMLLoader.load(getClass().getResource("/view/loginView.fxml"));
@@ -20,11 +21,13 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(parent));
         primaryStage.show();
     }
+
     // po zmaknięciu aplikacji
     @Override
-    public void stop(){
+    public void stop() {
         System.out.println("METODA STOP");
     }
+
     public static void main(String[] args) {
         launch(args);   // wywołanie metody statycznej uruchamiającej cykl życia aplikacji
     }
